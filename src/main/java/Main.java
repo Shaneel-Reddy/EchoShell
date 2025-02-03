bd62cf570ccb0ce837041805bf85b9df1bd2f750
Shaneel Reddy <shaneelreddy320@gmail.com>
2025-02-03 19:58:01 +0530

    feat: implement exit builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-        
+
         Scanner scanner = new Scanner(System.in);
         while(true)
         {
             System.out.print("$ ");
             String input = scanner.nextLine();
+            if (input.trim().equals("exit 0")) {
+                System.exit(0); 
+            }
             System.out.println(input + ": command not found");
         }
     }
