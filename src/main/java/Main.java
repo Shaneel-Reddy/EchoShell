3b021e6db1b597c694930842690fd0aa0be37ce8
Shaneel Reddy <shaneelreddy320@gmail.com>
2025-02-03 20:13:30 +0530

    feat: implement type builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,16 @@ public class Main {
             {
                 System.out.println(input.substring(5));
             }
+
+            else if(input.startsWith("type")) {
+                String command = input.substring(5).trim();
+                if (command.equals("echo") || command.equals("exit") || command.equals("type")) {
+                    System.out.println(command + " is a shell builtin");
+                } else {
+                    System.out.println(command + ": not found");
+                }
+            }
+
             else {
                 System.out.println(input + ": command not found");
             }
