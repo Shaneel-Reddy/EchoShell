0b27d1dbb6e26ba00e66780054ab6fa8b49bc43f
Shaneel Reddy <shaneelreddy320@gmail.com>
2025-02-15 18:50:03 +0530

    feat: implement append redirection support for commands

--- src/main/java/commands/CommandExecutor.java
+++ src/main/java/commands/CommandExecutor.java
@@ -13,6 +13,13 @@ public class CommandExecutor {
             System.out.println(command + ": command not found");
             return;
         }
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals(">") || args[i].equals("1>") || args[i].equals("2>") || args[i].equals(">>") || args[i].equals("1>>")) {
+                RedirectionExecutor.execute(command, args, i);
+                return;
+            }
+        }
         try {
             List<String> commandList = new ArrayList<>();
             commandList.add(command);
--- src/main/java/commands/EchoCommand.java
+++ src/main/java/commands/EchoCommand.java
@@ -1,7 +1,16 @@
 package commands;
 
 public class EchoCommand {
+
     public static void handle(String[] args) {
+
+    public static void handle(String command,String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals(">") || args[i].equals("1>") || args[i].equals("2>") || args[i].equals(">>") || args[i].equals("1>>")) {
+                RedirectionExecutor.execute(command, args, i);
+                return;
+            }
+        }
         StringBuilder output = new StringBuilder();
         for (String arg : args) {
             output.append(arg).append(" ");
