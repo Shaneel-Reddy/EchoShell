cdf641b8148d9950142c530ded69c1e2950d5e0d
Shaneel Reddy <shaneelreddy320@gmail.com>
2025-02-23 18:00:44 +0530

    feat:Builtin completion

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,19 @@
 import java.io.File;
 import java.util.List;
-import utils.ShellReader;
+import java.util.Scanner;
+import commands.CommandExecutor;
+import commands.EchoCommand;
+import commands.CdCommand;
+import commands.TypeCommand;
 import utils.Tokenizer;
-import commands.*;
-
 public class Main {
     private static File pwd = new File(System.getProperty("user.dir"));
 
     public static void main(String[] args) throws Exception {
-        ShellReader reader = new ShellReader();
-
+        Scanner scanner = new Scanner(System.in);
         while (true) {
-            String input = reader.readLine();
-            if (input == null) {
-                break;
-            }
-
-            input = input.trim();
+            System.out.print("$ ");
+            String input = scanner.nextLine().trim();
             if (input.equals("exit 0")) {
                 System.exit(0);
             }
@@ -29,8 +26,9 @@ public class Main {
             String command = tokens.getFirst();
             String[] commandArgs = tokens.subList(1, tokens.size()).toArray(new String[0]);
 
+
             switch (command) {
-                case "echo" -> EchoCommand.handle(command, commandArgs);
+                case "echo" -> EchoCommand.handle(command,commandArgs);
                 case "pwd" -> System.out.println(pwd.getAbsolutePath());
                 case "cd" -> pwd = CdCommand.handle(commandArgs, pwd);
                 case "type" -> TypeCommand.handle(commandArgs);
